583374faedfe5401fb33fc5ed289fad1eb9d3f37
ImDav1dReal <115368785+ImDav1dReal@users.noreply.github.com>
2023-03-19 15:26:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,2 @@
 
-document.title = "ImDav1d Packages - Downloads";
-
-const themeToggleButton = document.getElementById("theme-toggle");
-
-
-themeToggleButton.addEventListener("click", function() {
-
-  if (document.body.classList.contains("light-theme")) {
-    document.body.classList.remove("light-theme");
-    document.body.classList.add("dark-theme");
-  } else {
-    document.body.classList.remove("dark-theme");
-    document.body.classList.add("light-theme");
-    function switchTheme() {
-    const h4 = document.querySelector('h4')
-    h4.classList.toggle('dark-theme');
-    h4.classList.toggle('light-theme');
-}
-  }
-});
-
-
+document.title = "CatYaw - Cat1337";
